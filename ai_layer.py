31344b16d1852066bc4eb0ea0a16f3ce2c2eeea9
krushnanirmalkar <krushnanirmalkar@gmail.com>
2026-05-26 12:23:16 +0530

    added the ai layer

---
+++ ai_layer.py
@@ -0,0 +1,18 @@
+import os
+from openai import OpenAI
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def ask_ai(question: str) -> str:
+    if OPENAI_API_KEY is None:
+        return "No API key found"
+    else:
+        response = client.responses.create(
+            model="gpt-3.5-mini",
+            input=question
+        )
+        return response.output_text
+
+
+
+
